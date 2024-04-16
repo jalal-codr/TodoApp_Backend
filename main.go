d194bf4807990042470827f658dd0e7ddfda7789
jalal-codr <abduljalal849@gmail.com>
2024-04-16 20:15:41 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+type todo struct {
+	Email string
+}
+
+var todos = []todo{
+	{Email: "jalal@gmail.com"},
+	{Email: "jalal@gmail.com"},
+	{Email: "jalal@gmail.com"},
+	{Email: "jalal@gmail.com"},
+}
