6abb9f11c4144307d6fcedd0208cb60cc3f46dcc
jalal-codr <abduljalal849@gmail.com>
2024-04-16 21:07:50 +0100

    post route created

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
-	Email string
+	Email string `json:"Email"`
 }
 
 var todos = []todo{
@@ -9,4 +15,28 @@ var todos = []todo{
 	{Email: "jalal@gmail.com"},
 	{Email: "jalal@gmail.com"},
 	{Email: "jalal@gmail.com"},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func addTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
+
+	router.Run("localhost:9090")
 }
