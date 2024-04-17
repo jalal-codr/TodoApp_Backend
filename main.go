24146d3a3e0eb20edc89b49c4790912f757f2d07
jalal-codr <abduljalal849@gmail.com>
2024-04-17 18:28:14 +0100

    updates

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type todo struct {
@@ -21,6 +27,31 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func getTodo(context *gin.Context) {
+	email := context.Param("email")
+	todo, err := getTodoByEmail(email)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"Message": "todo no found"})
+	}
+	context.IndentedJSON(http.StatusOK, todo)
+
+}
+
+func getTodoByEmail(email string) (*[]todo, error) {
+	var mylist = []todo{}
+	for i, value := range todos {
+		if value.Email == email {
+			mylist = append(mylist, todos[i])
+			// return &todos[i], nil
+		}
+	}
+	if len(mylist) > 0 {
+		return &mylist, nil
+	}
+	return nil, errors.New("no todo found")
+}
+
 func addTodo(context *gin.Context) {
 	var newTodo todo
 	if err := context.BindJSON(&newTodo); err != nil {
@@ -33,9 +64,31 @@ func addTodo(context *gin.Context) {
 }
 
 func main() {
+	//Database conection
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Println("No .env file found")
+	// }
+	// uri := os.Getenv("MONGODB_URI")
+	// if uri == "" {
+	// 	log.Fatal("You must set your 'MONGODB_URI' environment variable")
+
+	// }
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://panda:panda@cluster0.sk272un.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	router := gin.Default()
 
 	router.GET("/todos", getTodos)
+	router.GET("/todo/:email", getTodo)
 	router.POST("/todos", addTodo)
 
 	router.Run("localhost:9090")
